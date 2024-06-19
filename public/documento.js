0077e3824660f534645c30dba3f6d7f12ba7550e
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-19 17:36:05 -0300

    feat: :sparkles: add: Capturando dados do frontend e enviando para o back pelo Socket.io

--- public/documento.js
+++ public/documento.js
@@ -1,2 +1,9 @@
 // eslint-disable-next-line no-unused-vars, no-undef
 const socket = io();
+
+// eslint-disable-next-line no-undef
+const textoEditor = document.getElementById('editor-texto');
+
+textoEditor.addEventListener('keyup', () => {
+  socket.emit('texto_editor', textoEditor.value);
+});
--- src/socket-back.js
+++ src/socket-back.js
@@ -2,4 +2,7 @@ import io from './servidor.js';
 
 io.on('connection', (socket) => {
   console.log('Um cliente se conectou!', socket.id);
+  socket.on('texto_editor', (texto) => {
+    console.log(texto);
+  });
 });
