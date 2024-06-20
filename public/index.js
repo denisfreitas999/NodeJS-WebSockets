5e2e5e34e5a7c6ab6a3691b51bfd3760667984ae
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-20 14:15:52 -0300

    refactor: :recycle: ref: Refatoração do código

--- public/index.js
+++ public/index.js
@@ -15,7 +15,7 @@ form.addEventListener('submit', (evento) => {
 function inserirLinkDocumento(nomeDocumento) {
   listaDocumentos.innerHTML
     += `<a 
-          href="documento.html?nome=${nomeDocumento}" 
+          href="/documento/documento.html?nome=${nomeDocumento}" 
           class="list-group-item list-group-item-action"
           id="documento-${nomeDocumento}"
         >
