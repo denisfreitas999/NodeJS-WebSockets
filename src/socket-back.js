b2a7202acd36466127629b14c5f760e78483038b
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-19 23:02:31 -0300

    feat: :sparkles: add: Implementada alteração dos dados no banco de dados

--- src/socket-back.js
+++ src/socket-back.js
@@ -6,20 +6,35 @@ async function encontrarDocumento(nome) {
   return documento;
 }
 
+async function atualizaDocumento(dto) {
+  const atualizacao = await DocumentoModel.updateOne(
+    {
+      nome: dto.nomeDocumento,
+    },
+    {
+      $set: {
+        texto: dto.texto,
+      },
+    },
+  );
+
+  return atualizacao;
+}
+
 io.on('connection', (socket) => {
   console.log('Um cliente se conectou!', socket.id);
   socket.on('selecionar_documento', async (nomeDocumento, devolverTexto) => {
     socket.join(nomeDocumento);
     const documento = await encontrarDocumento(nomeDocumento);
-    console.log(documento);
     if (documento) {
       devolverTexto(documento.texto);
     }
   });
   socket.on('texto_editor', async ({ texto, nomeDocumento }) => {
-    const documento = await encontrarDocumento(nomeDocumento);
-    if (documento) {
-      documento.texto = texto;
+    const atualizacao = await atualizaDocumento({ nomeDocumento, texto });
+
+    if (atualizacao.modifiedCount) {
+      atualizacao.texto = texto;
       socket.to(nomeDocumento).emit('texto_editor_clientes', texto);
     }
   });
