be2951c0469ff6304805c376ed48b6dd909de0d3
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-24 14:27:43 -0300

    feat: :sparkles: add:  Detecção da saída de pessoas das salas dos documentos

--- server/event/documentos.js
+++ server/event/documentos.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-shadow */
 import documentoController from '../controller/documentoController.js';
 import { adicionarConexao, obterUsuariosDocumento } from '../utils/conexoesDocumentos.js';
 
@@ -17,23 +18,29 @@ export default function registrarEventosDocumentos(socket, io) {
 
       devolverTexto(documento.texto);
     }
-  });
-  socket.on('texto_editor', async ({ texto, nomeDocumento }) => {
-    const atualizacao = await documentoController.atualizaDocumento({ nomeDocumento, texto });
 
-    if (atualizacao.modifiedCount) {
-      atualizacao.texto = texto;
-      socket.to(nomeDocumento).emit('texto_editor_clientes', texto);
-    }
-  });
-  socket.on('disconnect', (motivo) => {
-    console.log(`Cliente "${socket.id}" desconectado!
-    Motivo: ${motivo}`);
-  });
-  socket.on('excluir_documento', async (nomeDocumento) => {
-    const resultado = await documentoController.excluirDocumento(nomeDocumento);
-    if (resultado) {
-      io.emit('excluir_documento_sucesso', nomeDocumento);
-    }
+    socket.on('texto_editor', async ({ texto, nomeDocumento }) => {
+      const atualizacao = await documentoController.atualizaDocumento({ nomeDocumento, texto });
+
+      if (atualizacao.modifiedCount) {
+        atualizacao.texto = texto;
+        socket.to(nomeDocumento).emit('texto_editor_clientes', texto);
+      }
+    });
+    /* socket.on('disconnect', (motivo) => {
+      console.log(`Cliente "${socket.id}" desconectado!
+      Motivo: ${motivo}`);
+    }); */
+    socket.on('excluir_documento', async (nomeDocumento) => {
+      const resultado = await documentoController.excluirDocumento(nomeDocumento);
+      if (resultado) {
+        io.emit('excluir_documento_sucesso', nomeDocumento);
+      }
+    });
+
+    socket.on('disconnect', (motivo) => {
+      console.log(`Cliente "${socket.id}" desconectado!
+      Motivo: ${motivo}`);
+    });
   });
 }
